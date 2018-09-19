884d45002e1d92e5362582112cb8418868bc999a
Sadick <sadickjunior@gmail.com>
2018-09-19 12:44:38 +0300

    replace webpack with rollup

---
+++ rollup.config.js
@@ -0,0 +1,13 @@
+import { uglify } from "rollup-plugin-uglify";
+
+export default {
+  input: "src/index.js",
+  output: {
+    format: "umd",
+    name: "scoped-style",
+    file: "scoped-style.js",
+    dir: "dist/"
+  },
+  plugins: [uglify()],
+  sourcemap: true
+};
--- src/index.js
+++ src/index.js
@@ -1,45 +1,47 @@
-const scoped = function scoped(h) {
-  let index = 1;
-  return elem => (tags, ...fns) => (props, args) => {
-    let children = Array.isArray(args) ? args : props.children;
-    const className = `i${index++}`;
-    var sheet = document.head.appendChild(document.createElement("style"))
-      .sheet;
-
-    let styles = tags.reduce((style, tag, i) => {
-      return (style += tag + (fns[i] ? fns[i](props) : ""));
-    }, "");
-
-    const pseudoSelectorRegex = /(::?|>.*)\w+\s\{\W+[a-z:;\s\w"'-]+\}/gm;
-    const atRuleRegex = /@.*\W+[a-z:\s\w;-]+\}/gm;
-
-    const pseudoSelectors = styles.match(pseudoSelectorRegex) || [];
-    pseudoSelectors.forEach(pseudoSelector => {
-      let rule = "." + className + pseudoSelector;
-      sheet.insertRule(rule, sheet.cssRules.length);
-    });
-    styles = styles.replace(pseudoSelectorRegex, "");
-
-    const atRules = styles.match(atRuleRegex) || [];
-    styles = styles.replace(atRuleRegex, "");
-    sheet.insertRule(`.${className} {${styles}}`, sheet.cssRules.length);
-
-    atRules.forEach(atRule => {
-      let rule = atRule.match(/\{\W+[\w\s;:"'-]+\}+/gm);
-      let style = atRule.match(/@.*/) + "." + className + rule + "}";
-      sheet.insertRule(style, sheet.cssRules.length);
-    });
-    let attr = Object.assign({}, props);
-    attr.className = className
-    if ("class" in props) {
-      attr.className += ` ${props.class}`;
-    }
-    if (h.name === "createElementWithValidation") {
-      delete attr.class;
-      return h(elem, attr, children);
-    }
-    return h(elem, attr, children);
+var sheet = document.head.appendChild(document.createElement("style")).sheet;
+function scoped(h) {
+  var _id = 1;
+  return function(elem) {
+    return function(tags) {
+      var fns = [].slice.call(arguments);
+      fns.shift();
+      return function(props, children) {
+        var classID = "i" + _id;
+        children = children || props.children;
+        var styles = "";
+        for (var index = 0; index < tags.length; index++) {
+          styles += tags[index] + (fns[index] ? fns[index](props) : "");
+        }
+        var pseudoSelectorRegex = /(::?|>.*)\w+\s\{\W+[a-z:;\s\w"'-]+\}/gm;
+        var atRuleRegex = /@.*\W+[a-z:\s\w;-]+\}/gm;
+        var matches = styles.match(pseudoSelectorRegex) || [];
+        for (var index = 0; index < matches.length; index++) {
+          var rule = ".i" + _id + matches[index];
+          sheet.insertRule(rule, sheet.cssRules.length);
+        }
+        matches = styles.match(atRuleRegex) || [];
+        for (var index = 0; index < matches.length; index++) {
+          var rule = matches[index].match(/\{\W+[\w\s;:"'-]+\}+/gm);
+          var style = matches[index].match(/@.*/) + "." + classID + rule + "}";
+          sheet.insertRule(style, sheet.cssRules.length);
+        }
+        styles = styles.replace(pseudoSelectorRegex, "");
+        styles = styles.replace(atRuleRegex, "");
+        sheet.insertRule(
+          "." + classID + "{" + styles + "}",
+          sheet.cssRules.length
+        );
+        props.class = classID + " " + props.class || props.className;
+        var attr = Object.assign({}, props);
+        if (h.name === "createElementWithValidation") {
+          attr.className = attr.class;
+          delete attr.class;
+          return h(elem, attr, children);
+        }
+        return h(elem, attr, children);
+      };
+    };
   };
-};
+}
 
 export default scoped;
