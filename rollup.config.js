ae6399837af8e82a6d45534ec891991ab7e1a3d5
Sadick <sadickjunior@gmail.com>
2019-11-26 12:59:14 +0300

    Revert "Revert "TypeScript type declarations"" (#29)
    * Revert "Revert "TypeScript type declarations (#26)" (#28)"
    
    This reverts commit 239d8f46300c2ae796a82db23b55277f6e564938.
    
    * Update @types/index.d.ts
    
    * Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,13 @@
-import { uglify } from "rollup-plugin-uglify";
+import { uglify } from 'rollup-plugin-uglify';
 
 export default {
-  input: "src/index.js",
+  input: 'src/index.js',
   output: {
-    format: "umd",
-    name: "scoped-style",
-    file: "scoped-style.js",
-    dir: "dist/",
-    sourcemap: true
+    format: 'umd',
+    name: 'scoped-style',
+    file: 'scoped-style.js',
+    dir: 'dist/',
+    sourcemap: true,
   },
   plugins: [uglify()],
 };
